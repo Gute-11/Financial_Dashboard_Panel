eabf4256ef33520696bdd43bfd312fb5c1b4ea9b
Gute-11 <guga.f.martins09@gmail.com>
2025-08-26 23:20:46 -0300

    incrementos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 function NavBar() {
   return (
-    <header className="bg-gradient-to-r from-lime-300 to-lime-600 m-1 rounded-md shadow p-4">
+    <header className="bg-gradient-to-r from-lime-300 to-lime-600 rounded-md shadow p-4">
       <h1 className="text-center text-xl font-bold">Menu Principal</h1>
     </header>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,17 +4,50 @@ function SideBar() {
       <h2 className="text-xl font-semibold pb-16 text-center">Menu</h2>
       <ul className="space-y-8">
         <li>
-          <a href="#" className="hover:text-gray-300 border-l-2 p-1">
+          <a href="#" className="hover:text-gray-300 border-l-2 p-2 flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 inline-block"
+            >
+              <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+              <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+            </svg>
             Menu Principal
           </a>
         </li>
         <li>
-          <a href="#" className="hover:text-gray-300 border-l-2 p-1">
+          <a href="#" className="hover:text-gray-300 border-l-2 p-2 flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 inline-block"
+            >
+              <path
+                fill="evenodd"
+                d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
+                clip="evenodd"
+              />
+            </svg>
             Receitas
           </a>
         </li>
         <li>
-          <a href="#" className="hover:text-gray-300 border-l-2 p-1">
+          <a href="#" className="hover:text-gray-300 border-l-2 p-2 flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 inline-block"
+            >
+              <path
+                fill="evenodd"
+                d="M1.72 5.47a.75.75 0 0 1 1.06 0L9 11.69l3.756-3.756a.75.75 0 0 1 .985-.066 12.698 12.698 0 0 1 4.575 6.832l.308 1.149 2.277-3.943a.75.75 0 1 1 1.299.75l-3.182 5.51a.75.75 0 0 1-1.025.275l-5.511-3.181a.75.75 0 0 1 .75-1.3l3.943 2.277-.308-1.149a11.194 11.194 0 0 0-3.528-5.617l-3.809 3.81a.75.75 0 0 1-1.06 0L1.72 6.53a.75.75 0 0 1 0-1.061Z"
+                clip="evenodd"
+              />
+            </svg>
             Despesas
           </a>
         </li>
