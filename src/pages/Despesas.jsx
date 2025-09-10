0ea91e6551afe74f1ed3001261b54ffbffe1d6c5
Gute-11 <guga.f.martins09@gmail.com>
2025-09-09 23:18:20 -0300

    add expenses

--- src/pages/Despesas.jsx
+++ src/pages/Despesas.jsx
@@ -11,18 +11,55 @@ function Receitas() {
       <div className="flex-1 flex flex-col">
         <NavBar />
 
-        <main className="p-4 pt-24">
+        <main className="p-4 w-auto pt-24">
           <div className="pt-4">
             <h1 className="text-2xl font-bold text-center">
-              Aba de adição de Despesas em processo de criação!
+              Adicione seus Gastos!
             </h1>
-            <div className="shadow p-2 flex justify-center">
+
+            <div className="shadow rounded-lg bg-slate-50 p-2 flex flex-col gap-4 h-auto">
+              
+              <div className="mb-4">
+                <label className="block mb-1">Descrição:</label>
+                <input className="w-96 shadow border border-gray-300 rounded-lg px-3 py-2
+                focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-300 ease-in-out" type="text"
+                placeholder="Digite a descrição"/>
+              </div>
+
+              <div className="mb-4">
+                <label className="block mb-1">Valor:</label>
+                <input className="border shadow border-gray-300 rounded-lg px-3 py-2
+                focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-300 ease-in-out" type="number"
+                placeholder="0"/>
+              </div>
+
+              <div className="mb-4 items-center gap-4">
+                <label className="block mb-1">Data:</label>
+                <input className="border shadow border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-300 ease-in-out" type="date"/>
+              </div>
+
+              <div className="mb-4 items-center gap-4">
+                <label className="block mb-1">Categoria:</label>
+                <select className="w-72 shadow border border-gray-300 rounded-lg focus:ring-2
+                focus:ring-blue-500 transition transform hover:scale-105 duration-300 ease-in-out px-3 py-2">
+                  <option>Alimentos</option>
+                  <option>Compras</option>
+                  <option>Boleto</option>
+                  <option>Dividas</option>
+                  <option>Mensalidade</option>
+                  <option>Combustível</option>
+                  <option>Outros</option>
+                </select>
+              </div>
+
+              <div className="flex w-full justify-center">
               <button
-                className="px-16 py-4 bg-red-600 text-white rounded
-               transition-all duration-300 ease-linear hover:scale-105"
+                className="px-12 h-16 bg-red-600 text-white rounded
+                transition-all duration-300 ease-linear hover:scale-105 focus:outline-none"
               >
-                Adicionar Ganho
+                Adicionar Gastos
               </button>
+              </div>
             </div>
           </div>
         </main>
--- src/pages/Receitas.jsx
+++ src/pages/Receitas.jsx
@@ -20,26 +20,26 @@ function Receitas() {
             <div className="shadow rounded-lg bg-slate-50 p-2 flex flex-col gap-4 h-auto">
               
               <div className="mb-4">
-                <label className="block mb-1">Descrição</label>
+                <label className="block mb-1">Descrição:</label>
                 <input className="w-96 shadow border border-gray-300 rounded-lg px-3 py-2
                 focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-300 ease-in-out" type="text"
                 placeholder="Digite a descrição"/>
               </div>
 
               <div className="mb-4">
-                <label className="block mb-1">Valor</label>
+                <label className="block mb-1">Valor:</label>
                 <input className="border shadow border-gray-300 rounded-lg px-3 py-2
                 focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-300 ease-in-out" type="number"
                 placeholder="0"/>
               </div>
 
               <div className="mb-4 items-center gap-4">
-                <label className="block mb-1">Data</label>
+                <label className="block mb-1">Data:</label>
                 <input className="border shadow border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-300 ease-in-out" type="date"/>
               </div>
 
               <div className="mb-4 items-center gap-4">
-                <label className="block mb-1">Categoria</label>
+                <label className="block mb-1">Categoria:</label>
                 <select className="w-72 shadow border border-gray-300 rounded-lg focus:ring-2
                 focus:ring-blue-500 transition transform hover:scale-105 duration-300 ease-in-out px-3 py-2">
                   <option>Salário</option>
