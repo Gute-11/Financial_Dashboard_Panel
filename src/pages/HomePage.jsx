12e02449fd4cfd0e46706e9e47f2e7c68a095e20
Gute-11 <guga.f.martins09@gmail.com>
2025-09-15 23:18:05 -0300

    Home page Improvements

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import fotoLogo from "../images/Logo.png";
+import fotoHomePage from "../images/imagemHomePage.png";
 import "../index.css";
 
 function HomePage() {
   return (
-    <div className="">
+    <div>
       <header
         className="bg-white h-28 gap-3 flex items-center
          fixed top-0 w-screen px-4"
@@ -12,13 +13,18 @@ function HomePage() {
           src={fotoLogo}
           alt="Foto Logo"
           className=" rounded-full
-            object-cover w-16 h-16 cursor-pointer border-gray-300
+            object-cover w-20 h-20 cursor-pointer border-gray-300
             transition-all duration-300 ease-linear hover:scale-110"
         />
-        <h1>FLEXBOARD</h1>
-        <a>alguma coisa</a>
+        <a href="">FLEXBOARD</a>
+        <a href="">alguma coisa</a>
+        <a href="">Sobre Nós</a>
+        <button className="bg-green-400 py-2 ml-auto text-white px-6 rounded-md">Conectar</button>
+        <p>ou</p>
+        <button className="py-2 px-6 rounded-md border-green-400 border-2">Cadastrar</button>
       </header>
-      <div className="pt-28 h-screen flex w-screen background-pattern">
+
+      <div className="pt-28 h-full md:h-screen flex background-pattern">
         <main className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 py-8">
           <div className="justify-center flex flex-col gap-4 pl-4">
             <h1 className="text-white text-4xl font-bold">
@@ -29,15 +35,22 @@ function HomePage() {
               Importa
             </p>
             <p className="text-white font-semibold">
-              &quot;Agora, consigo visualizar tudo de forma clara e organizada. O
-              FLEXBOARD me deu o controle que eu precisava!&quot; — Gutemberg F. M.,
-              Cliente Satisfeito
+              &quot;Agora, consigo visualizar tudo de forma clara e organizada.
+              O FLEXBOARD me deu o controle que eu precisava!&quot; — Gutemberg
+              F. M., Cliente Satisfeito
             </p>
-            <button className="mt-4 bg-white text-green-500 py-2 px-6 rounded-lg">COMEÇAR!</button>
+            <button className="mt-4 bg-white text-green-500 py-2 px-6 rounded-lg">
+              COMEÇAR!
+            </button>
           </div>
 
-          <div className="justify-center flex flex-col">
-            <h1>COLUNA COLUNA COLUNA IMAGEM AQUI!</h1>
+          <div className="flex flex-col">
+            <img
+              src={fotoHomePage}
+              alt="Foto Home Page"
+              className="object-cover m-auto justify-center h-80 w-80
+              rounded-3xl ease-linear"
+            />
           </div>
         </main>
       </div>
